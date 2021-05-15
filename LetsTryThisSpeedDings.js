5b20b468efbd6561eb841f681ad34dbb7602cba4
Timmmmo <84082025+Timmmmo@users.noreply.github.com>
2021-05-15 15:51:37 +0200

    Add files via upload
    in der .js steht mein aktueller code für den speedtest drin.
    Es funktioniert so: 
    -es wird ein bild runtergeladen (dabei wird die startzeit gespeichert)
    -die größe des bildes muss bekannt sein
    -in dem moment wo es fertig geladen hat, wird die endzeit gespeichert
    -die dauer des loads wird durch das verrechnen der start und endzeit berechnet, und danach mit der größe des bilds
    -der daraus entstehende wert ist die downloadgeschwindigkeit

---
+++ LetsTryThisSpeedDings.js
@@ -0,0 +1,50 @@
+var Bild = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg";
+var Größe = "10174706";
+
+//var StartZeit, EndZeit;
+var Download = new Image();
+
+
+function Berechnung(){
+    var länge = (EndZeit - StartZeit) / 1000;
+    console.log("länge: " + länge);
+    var geladen = Größe * 8;
+
+    var bps = ( geladen / länge ).toFixed(2);
+    console.log("länge der Zeit: " + bps);
+    console.log("geladene größe: " + bps);
+    console.log("byte pro Sekunde: " + bps);
+    var anzeigegeschw = speed(bps);
+    console.log(anzeigegeschw);
+    var ergebnis = "Your connection speed is" + anzeigegeschw.value + " " + anzeigegeschw.units;
+
+    document.getElementById('ergebnis').innerHTML=ergebnis;
+    console.log(ergebnis);
+    //document.getElementById('label').innerHTML=ergebnis;
+}
+
+function speed( bitsProSekunde ){
+    var kBps = (bitsProSekunde / 1024).toFixed(2);
+    if ( kBps <= 1 ) return { value: bitsProSekunde, units: "Bps" };
+    var mBps = (kBps / 1024).toFixed(2);
+    if ( mBps <= 1 ) return { value: kBps, units: "KBps" };
+    else return { value: mBps, units: "MBps" };
+}
+
+
+function go(){
+
+    for(let i = 0; i <= 10; i++){
+    var download = new Image();
+    download.onload = function () {
+        EndZeit = (new Date()).getTime();
+        console.log("endzeit: "+ EndZeit);
+        Berechnung();
+    }
+    StartZeit = (new Date()).getTime();
+    console.log("startzeit: "+ StartZeit);
+    var cacheBuster = "?nnn=" + StartZeit;
+    download.src = Bild + cacheBuster;
+
+    }
+}
